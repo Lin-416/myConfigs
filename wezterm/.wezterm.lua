257c3bc93ca819bff3d950eb00faa6d7995ece34
Lin-416 <lijianlin050416@outlook.com>
2025-10-29 11:20:41 +0800

    add blur

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,8 @@ local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 35
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.01
+config.macos_window_background_blur = 64
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
